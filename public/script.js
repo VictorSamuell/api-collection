717ef3df6124b5abb33f34c2af5b471c7594cb84
VictorSamuell <victor.samuel1@aluno.ifsp.edu.br>
2025-01-14 23:48:47 -0300

    changes in script.js

--- public/script.js
+++ public/script.js
@@ -56,31 +56,3 @@ document.addEventListener('DOMContentLoaded', () => {
     
   });
 });
-
-
-
-/*
-document.getElementById('search-form').addEventListener('submit', async (event) => {
-    event.preventDefault(); // Evitar o envio tradicional do formulário
-    const pokemonName = document.getElementById('pokemon-name').value.toLowerCase();
-    const pokemonInfoDiv = document.getElementById('pokemon-info');
-  
-    try {
-      const response = await fetch(`/api/pokemon/${pokemonName}`);
-      const data = await response.json();
-  
-      if (data.error) {
-        pokemonInfoDiv.innerHTML = `<p>${data.error}</p>`;
-      } else {
-        pokemonInfoDiv.innerHTML = `
-          <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
-          <img src="${data.sprites.front_default}" alt="${data.name}">
-          <p>Altura: ${data.height} decímetros</p>
-          <p>Peso: ${data.weight} hectogramas</p>
-        `;
-      }
-    } catch (error) {
-      pokemonInfoDiv.innerHTML = '<p>Erro ao buscar o Pokémon. Tente novamente.</p>';
-    }
-  });
-  */
